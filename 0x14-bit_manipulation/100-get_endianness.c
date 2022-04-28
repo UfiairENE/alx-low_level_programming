4ae5ca9728a878e087f91e10be2d42b6b816fcfa
Ufia Irene <89124723+UfiairENE@users.noreply.github.com>
2022-04-28 22:35:49 +0100

    Create 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - Function that checks the endianness
+ *
+ * Return: nothing
+ */
+int get_endianness(void)
+{
+	unsigned int i = 0;
+	char *c = (char *)&i;
+
+	if (*c)
+		return (0);
+	else
+		r
